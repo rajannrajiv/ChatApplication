0b23445392204f326e1d070480d880937de41fd3
rajiv <rs481441@gmail.com>
2020-07-14 18:08:36 +0530

    Adding Beans to the main class and a service class as MessageService

--- src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -2,12 +2,32 @@ package com.udacity.jwdnd.c1.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
+	}
+	@Bean
+	public String message()
+	{
+		System.out.println("creating message bean");
+		return "Hello Spring!";
+	}
+
+	@Bean
+	public String UpperCaseMessage(MessageService msg)
+	{
+		System.out.println("Creating UpperCasemessage bean");
+		return msg.uppercase();
+	}
+	@Bean
+	public String LowerCaseMessage(MessageService msg)
+	{
+		System.out.println("Creating lowerCasemessage bean");
+		return msg.lowercase();
 	}
 
 }
